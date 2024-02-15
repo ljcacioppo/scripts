0d51d8c902b09a180714ca507e218f8d9afeded3
Leonardo Cacioppo <44585848+ljcacioppo@users.noreply.github.com>
2024-02-15 11:33:16 -0600

    Update generate_email_list_from_computer_group.sh
    Updated to bearer token authentication using client ID and secret

--- generate_email_list_from_computer_group.sh
+++ generate_email_list_from_computer_group.sh
@@ -2,26 +2,68 @@
 
 #This script is a template to grab the email addresses for users in a particular Computer Group within Jamf and create a CSV from them
 
-jamfURL=""
-authorization=""
+# Define Variables
+jamfProURL=""
+client_id=""
+client_secret=""
 #Specify the Computer Group Number
 ID=""
 #Specify the output file for the CSV
 outputFile="/path/to/output.csv"
 
+getAccessToken() {
+	response=$(curl --silent --location --request POST "${jamfProURL}/api/oauth/token" \
+		--header "Content-Type: application/x-www-form-urlencoded" \
+		--data-urlencode "client_id=${client_id}" \
+		--data-urlencode "grant_type=client_credentials" \
+		--data-urlencode "client_secret=${client_secret}")
+	access_token=$(echo "$response" | plutil -extract access_token raw -)
+	token_expires_in=$(echo "$response" | plutil -extract expires_in raw -)
+	token_expiration_epoch=$(($current_epoch + $token_expires_in - 1))
+}
+
+checkTokenExpiration() {
+	current_epoch=$(date +%s)
+	if [[ token_expiration_epoch -ge current_epoch ]]
+	then
+		echo "Token valid until the following epoch time: " "$token_expiration_epoch"
+	else
+		echo "No valid token available, getting new token"
+		getAccessToken
+	fi
+}
+
+invalidateToken() {
+	responseCode=$(curl -w "%{http_code}" -H "Authorization: Bearer ${access_token}" $jamfProURL/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	if [[ ${responseCode} == 204 ]]
+	then
+		echo "Token successfully invalidated"
+		access_token=""
+		token_expiration_epoch="0"
+	elif [[ ${responseCode} == 401 ]]
+	then
+		echo "Token already invalid"
+	else
+		echo "An unknown error occurred invalidating the token"
+	fi
+}
+
+#generate a Bearer Token and set it as a variable
+checkTokenExpiration
+
 #Obtain computer IDs for computers in Jamf Pro from the Smart Group
 computerIDs=$( /usr/bin/curl -s \
---header "authorization: Basic $authorization" \
+--header "authorization: Bearer $access_token" \
 --header "Accept: text/xml" \
 --request GET \
-$jamfURL/computergroups/id/$ID | xmllint --xpath '/computer_group/computers' - | xmllint --format - | awk -F '[<>]' '/<id>/{print $3}' )
+$jamfProURL/computergroups/id/$ID | xmllint --xpath '/computer_group/computers' - | xmllint --format - | awk -F '[<>]' '/<id>/{print $3}' )
 
 for computerID in $computerIDs;do
 	specificID=$(/usr/bin/curl -s \
-	--header "authorization: Basic $authorization" \
+	--header "authorization: Bearer $access_token" \
 	--header "Accept: text/xml" \
 	--request GET \
-	$jamfURL/computers/id/$computerID )
+	$jamfProURL/computers/id/$computerID )
 	userEmail=$( echo $specificID | xmllint --xpath '/computer/location/email_address/text()' - )
 	
 	if [[ ! -e $outputFile ]];then
@@ -33,3 +75,7 @@ for computerID in $computerIDs;do
 		echo $userEmail", " >> $outputFile
 	fi
 done
+
+#Invalidate Token
+invalidateToken 
+
